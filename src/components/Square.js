d8714b8039ed6d43cdd68d388079be32a770a558
Kim Carpico <kcarpico@gmail.com>
2021-09-15 09:57:54 -0700

    created boxes for tic-tac-toe, created Board and Square components

---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Square = ({ value }) => {
+
+    // console.log(props);
+    return (
+        <div>
+            <button type="button">{ value }</button>
+        </div>
+    )
+}
+
+export default Square
