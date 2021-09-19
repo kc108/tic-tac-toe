e8f44b52d2bb2b6b6dcdc5b0799e5d0d17c67555
Kim Carpico <kcarpico@gmail.com>
2021-09-19 09:29:19 -0700

    refactored code, moved state to App.js and formed logic to display winner on board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,37 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Square from './Square'
 
-const Board = () => {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    console.log(board);
-
-    const [isNext, setIsNext] = useState(false);
-    console.log('board rerender');
-
-    const handleSquareClick = (position) => {
-        // To Update to a Value state
-        // setBoard(5);
-
-        // If the board position exists, has an 'X' or 'O' then it will exit this function. Rather than not being able to toggle between 'X' and 'O'
-        if(board[position]) {
-            return;
-        }
-
-        setBoard((prev) => {
-
-            return prev.map((square, pos) => {
-
-                if(pos === position) {
-                    return isNext ? 'X' : '0';
-                }
-                    return square;
-            })
-
-        })
-
-        setIsNext((prev) => !prev);
-    };
-
+const Board = ({ board, handleSquareClick }) => {
+    
     const renderSquare = (position) => {
         return <Square value={board[position]} onClick={() => handleSquareClick(position)}/>
     }
