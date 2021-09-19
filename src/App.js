0db0c3f66a8a4d0aef9f25aad02289d89abd112b
Kim Carpico <kcarpico@gmail.com>
2021-09-19 09:52:48 -0700

    transformed state to game history

--- src/App.js
+++ src/App.js
@@ -6,18 +6,35 @@ import "./styles/root.scss"
 
 const App = () => {
 
-  const [board, setBoard] = useState(Array(9).fill(null));
-  // console.log(board);
+  const [history, setHistory] = useState([ {board: Array(9).fill(null), isXNext: true},
+   ]);
 
-  const [isXNext, setIsXNext] = useState(false);
-  // console.log('board rerender');
+  // Used to set Index of history Array of Objects above 
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const winner = calculateWinner(board);
+  const current = history[currentMove]
+
+  console.log('history', history)
+
+  // Changed this to history when implementing logic for game history
+  // const [board, setBoard] = useState(Array(9).fill(null));
+  // // console.log(board);
+
+  // removed this for implementing game history
+  // const [isXNext, setIsXNext] = useState(false);
+  // // console.log('board rerender');
+
+  // Changed to below to reference 
+  // const winner = calculateWinner(board);
   // console.log(winner);
+
+  const winner = calculateWinner(current.board);
 
   const message = winner 
     ? `Winner is ${winner}` 
-    : `Next player is ${isXNext ? 'X' : 'O'}`;
+    // Changed below line to implement Game History Logic
+    // : `Next player is ${isXNext ? 'X' : 'O'}`;
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
 
       const handleSquareClick = (position) => {
@@ -25,30 +42,48 @@ const App = () => {
           // setBoard(5);
 
           // If the board position exists, has an 'X' or 'O' then it will exit this function. Rather than not being able to toggle between 'X' and 'O'
-          if(board[position] || winner) {
+          // Changed to below to implement 'Game History Logic'
+          // if(board[position] || winner) {
+          //     return;
+          // }
+
+          if(current.board[position] || winner) {
               return;
           }
 
-          setBoard((prev) => {
+          // Changed to 'setHistory' as part of 'Game History' logic implementation
+          // setBoard((prev) => {
+          
+          setHistory((prev) => {
 
-              return prev.map((square, pos) => {
+            const last = prev[prev.length - 1];
 
+              // changed when implementing 'Game History' logic
+              // return prev.map((square, pos) => {
+              const newBoard = last.board.map((square, pos) => {
                   if(pos === position) {
-                      return isXNext ? 'X' : '0';
+                      // changed when implement 'Game History' logic
+                      // return isXNext ? 'X' : '0';
+                      return last.isXNext ? 'X' : '0';
                   }
                       return square;
-              })
+              });
 
-          })
+              return prev.concat({ board: newBoard, isXNext: !last.isXNext })
+          });
 
-          setIsXNext((prev) => !prev);
+          // No longer need this when implementing logic for 'Game History Logic'
+          // setIsXNext((prev) => !prev);
+          setCurrentMove(prev => prev + 1);
       };
 
       return (
         <div className="app">
           <h1>TIC TAC TOE</h1>
           <h2>{ message }</h2>
-          <Board board={board} handleSquareClick={handleSquareClick} />
+          {/* changed when implementing 'Game History' logic */}
+          {/* <Board board={board} handleSquareClick={handleSquareClick} /> */}
+          <Board board={current.board} handleSquareClick={handleSquareClick} />
         </div>
       );
     };
